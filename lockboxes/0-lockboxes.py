0283e0aad182ceb36d945d4421b12cd86608ca58
RuslanHamidov <rhamidov202@gmail.com>
2024-02-28 18:36:53 +0400

    main changes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,3 +3,24 @@
 '''
 Lockboxes algorithm
 '''
+
+
+
+def boxCheck(box_list):
+    bool_list = []
+    for array in box_list:
+        if (len(array)):
+            bool_list.append(True)
+        else:
+            bool_list.append(False)
+    
+    all_except_last = bool_list[:-1]
+    
+    print(all_except_last)
+    for boolean in all_except_last:
+        if(all(all_except_last) == True):
+            return True
+        else:
+            return False
+            
+    
