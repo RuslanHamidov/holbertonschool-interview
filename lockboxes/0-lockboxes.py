edb11cfec1cd4def6eb976d261b490b9f46e1509
RuslanHamidov <rhamidov202@gmail.com>
2024-02-29 15:48:41 +0400

    add main changes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,13 +4,21 @@
 Lockboxes algorithm
 '''
 
-def canUnlockAll (box_list):
-    bool_list = []
-    for array in box_list:
-        if (len(array)):
-            bool_list.append(True)
-        else:
-            bool_list.append(False)
-    all_except_last = bool_list[:-1]
-    for boolean in all_except_last:
-        return all(all_except_last) is True
+def canUnlockAll(boxes):
+    index_counter = 0
+    box_open = []
+    for lists in boxes:
+        for elements in lists:
+            index_counter = 0
+            for index_counter in range(1, len(boxes)):  
+                if elements == index_counter:
+                    box_open.append('opened')
+                    break
+
+    if(len(box_open) == len(boxes)-1):
+        print(True)
+    else:
+        print(False)
+
+boxes = [[5], [4], [1], [2], []]
+
