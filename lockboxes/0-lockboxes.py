07545cb892151caad16cfb91774793acf5bb1d9f
RuslanHamidov <rhamidov202@gmail.com>
2024-02-29 19:09:46 +0400

    new

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,11 +6,11 @@ Lockboxes algorithm
 
 
 def canUnlockAll(boxes):
-  unlocked = [False] * len(boxes)
-  unlocked[0] = True
-  for i, box_keys in enumerate(boxes):
-    for key in box_keys:
-      if 0 <= key < len(boxes) and not unlocked[key]:
-        unlocked[key] = True
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+    for i, box_keys in enumerate(boxes):
+        for key in box_keys:
+            if 0 <= key < len(boxes) and not unlocked[key]:
+                unlocked[key] = True
 
-  return all(unlocked)
+    return all(unlocked)
