039c368bc9e43ab72a3a3c9b5de2398d4e860bef
RuslanHamidov <rhamidov202@gmail.com>
2024-02-29 19:03:40 +0400

    new

--- lockboxes/test.py
+++ lockboxes/test.py
@@ -13,3 +13,20 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+
+def canUnlockAll(boxes):
+    index_counter = 0
+    box_open = set()
+    for lists in boxes:
+        for elements in lists:
+            index_counter = 0
+            for index_counter in range(1, len(boxes)):
+                if elements == index_counter:
+                    box_open.add(elements)
+                    break
+    list(box_open)
+    if(len(box_open) == len(boxes) - 1):
+        return True
+    else:
+        return False
