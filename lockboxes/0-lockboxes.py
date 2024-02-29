43f318a281ba32d25561b0f9708800eb6dc6f7b9
RuslanHamidov <rhamidov202@gmail.com>
2024-02-29 19:37:53 +0400

    new

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,11 +6,12 @@ Lockboxes algorithm
 
 
 def canUnlockAll(boxes):
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True
-    for i, box_keys in enumerate(boxes):
-        for key in box_keys:
-            if 0 <= key < len(boxes) and not unlocked[key]:
-                unlocked[key] = True
-
-    return all(unlocked)
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+    return True
--- lockboxes/test.py
+++ lockboxes/test.py
@@ -1,4 +1,4 @@
-def canUnlockAll(boxes):
+""" def canUnlockAll(boxes):
   box_open = set()
   for i, box in enumerate(boxes):
     if any(i in inner_box for inner_box in boxes[:i] + boxes[i+1:]):
@@ -29,4 +29,28 @@ def canUnlockAll(boxes):
     if(len(box_open) == len(boxes) - 1):
         return True
     else:
-        return False
+        return False """
+        
+        
+def canUnlockAll(boxes):
+    """ Method that determines if all boxes can be opened """
+
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+
+    return True
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))       
