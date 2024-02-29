8c129b5f96fa0b92a6270b601db016a70db7f7c5
RuslanHamidov <rhamidov202@gmail.com>
2024-02-29 16:25:52 +0400

    new

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,13 +4,14 @@
 Lockboxes algorithm
 '''
 
-def canUnlockAll(boxes):
+
+def canUnlockAll (boxes):
     index_counter = 0
     box_open = []
     for lists in boxes:
         for elements in lists:
             index_counter = 0
-            for index_counter in range(1, len(boxes)):  
+            for index_counter in range (1, len(boxes)):  
                 if elements == index_counter:
                     box_open.append('opened')
                     break
