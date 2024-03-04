c80ad0c0d65c34e1b8f99f8eb29029c7bb485b31
RuslanHamidov <rhamidov202@gmail.com>
2024-03-04 17:41:03 +0400

    c algorithms

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,30 +1,25 @@
 #include <stdlib.h>
 #include "lists.h"
 
-listint_t *insert_node(listint_t **head, int num)
+listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *current;
-    listint_t *new_node;
-    listint_t *prev;
-
-    current = *head;
-    prev = NULL;
-    new_node = malloc(sizeof(listint_t));
+    listint_t *new_node = malloc(sizeof(listint_t));
+    listint_t *current = *head;
+    listint_t *prev = NULL;
 
     if (new_node == NULL)
         return (NULL);
 
-    new_node->n = num;
+    new_node->n = number;
     new_node->next = NULL;
 
-    if (*head == NULL || (*head)->n >= num){
-        new_node->next = *head
+    if (*head == NULL || (*head)->n >= number) {
+        new_node->next = *head;
         *head = new_node;
         return (new_node);
     }
 
-    
-    while (current && current->n < num) {
+    while (current && current->n < number) {
         prev = current;
         current = current->next;
     }
