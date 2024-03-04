c25c3ed8cc236a3aeb5ab94fb13eb7d611e45950
RuslanHamidov <rhamidov202@gmail.com>
2024-03-04 13:51:11 +0400

    c algorithms

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *insert_node(listint_t **head, const int n)
 {
     listint_t *new;
     listint_t *current;
