b533658c710e00564ce9077f16776874ade01077
Ruslan Hamidov <124673207+RuslanHamidov@users.noreply.github.com>
2024-06-21 19:49:38 +0400

    Update 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,49 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
-const async = require('async');
+const arg = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${arg}`;
 
-if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.log(`Error: ${response.statusCode}`);
-    return;
-  }
-
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-
-  async.eachSeries(characters, (characterUrl, callback) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return callback(error);
-      }
-
-      if (response.statusCode !== 200) {
-        console.log(`Error: ${response.statusCode}`);
-        return callback(new Error(`Error: ${response.statusCode}`));
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-      callback();
-    });
-  }, (err) => {
-    if (err) {
-      console.error('Failed to fetch character names in order:', err);
-    }
+const charNames = (characters, i = 0) => {
+  if (i === characters.length) return;
+  request(characters[i], (error, response, body) => {
+    if (error) throw error;
+    // Convert a string of characters JSON to a JavaScript object and print it
+    console.log(JSON.parse(body).name);
+    // Call recursively the function charNames and increment it to pass to the next character
+    charNames(characters, i + 1);
   });
+};
+
+// Request to the API to recover movie information
+request(url, function (error, response, body) {
+  if (error) throw error;
+  // Extract URL of characters from the body JSON response and store it in a var char
+  const char = JSON.parse(body).characters;
+  // Call the function charNames to print the characters
+  charNames(char);
 });
