5e32e0d16d77bc25235285a6e841c7ec90c6522e
Ruslan Hamidov <124673207+RuslanHamidov@users.noreply.github.com>
2024-09-05 19:42:28 +0400

    Add files via upload

---
+++ making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Determine the fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Return the fewest number of coins needed to meet total
+    """
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
+        return 0
+
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    return temp_value if total == 0 else -1
